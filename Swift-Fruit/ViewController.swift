c7fcdd4cc005dbce6e7380dfbe3c31efe233a7d1
chenqitao <chenqitao520@qq.com>
2016-04-04 16:04:04 +0800

    first

---
+++ Swift-Fruit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Swift-Fruit
+//
+//  Created by chenqitao on 16/3/21.
+//  Copyright © 2016年 陈齐涛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+  
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
