c4dfbb4096221e6be6e6c988f41936cfb00506f0
chenqitao <chenqitao520@qq.com>
2016-04-14 19:17:16 +0800

    first

---
+++ Swift-Fruit/Class/AD/ADViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ADViewController.swift
+//  Swift-Fruit
+//
+//  Created by chenqitao on 16/4/6.
+//  Copyright © 2016年 陈齐涛. All rights reserved.
+//
+
+import UIKit
+
+class ADViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("yes" )
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
